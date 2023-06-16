3f63210f1216c87d04872dd65872165d9324b107
Ryu1845 <ryu@tpgjbo.xyz>
2023-06-16 19:31:36 +0200

    add masked self-attention to clvp

--- tortoise/clvp.py
+++ tortoise/clvp.py
@@ -1,11 +1,11 @@
+import math
 from collections import OrderedDict
 from dataclasses import dataclass
 
 import torch
 import torch.nn.functional as F
+from einops import rearrange
 from torch import einsum, nn
-
-from tortoise.gpt import CausalSelfAttention
 
 
 def masked_mean(t, mask):
@@ -36,13 +36,57 @@ class GEGLU(nn.Module):
         return x * F.gelu(gates)
 
 
+class SelfAttention(nn.Module):
+    """
+    A vanilla multi-head masked self-attention layer with a projection at the end.
+    It is possible to use torch.nn.MultiheadAttention here but I am including an
+    explicit implementation here to show that there is nothing too scary here.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.attn_pdrop)
+        self.resid_dropout = nn.Dropout(config.resid_pdrop)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+    def forward(self, x, mask=None):
+        B, T, C = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        if mask is not None:
+            mask = rearrange(mask, "b j -> b () () j")
+            att = att.masked_fill(mask, float("-inf"))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
+
+
 class Block(nn.Module):
     """a Transformer block with scale and geglu"""
 
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
-        self.attn = CausalSelfAttention(config)
+        self.attn = SelfAttention(config)
         self.scale_attn = nn.Parameter(torch.zeros(1, 1, config.n_embd).fill_(0.1))
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = nn.Sequential(
@@ -58,7 +102,7 @@ class Block(nn.Module):
         self.scale_mlp = nn.Parameter(torch.zeros(1, 1, config.n_embd).fill_(0.1))
 
     def forward(self, x, mask=None):
-        x = x + self.attn(self.ln_1(x)) * self.scale_attn
+        x = x + self.attn(self.ln_1(x), mask=mask) * self.scale_attn
         x = x + self.mlp(self.ln_2(x)) * self.scale_mlp
         return x
 
