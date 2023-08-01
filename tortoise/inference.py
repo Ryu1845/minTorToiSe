dcc134e63a2c88e6006ef87fe313d1da67b20e5a
Ryu1845 <ryu@tpgjbo.xyz>
2023-08-01 19:32:16 +0200

    inference: adapt to changes in modules

--- tortoise/inference.py
+++ tortoise/inference.py
@@ -1,3 +1,4 @@
+import os.path
 from typing import List
 
 import torch
@@ -21,27 +22,33 @@ class Inference:
         self.tokenizer = Tokenizer()
         self.clvp = CLVP.from_pretrained().eval()
         self.diffuser = SpacedDiffuser.from_pretrained().eval()
-        self.vocoder = UnivNetGenerator.from_pretrained().eval()
+        self.vocoder = UnivNetGenerator.from_pretrained().eval(inference=True)
         self.calm_token = 83  # token for coding silence # TODO: don't hardcode
 
     @torch.inference_mode()
     def __call__(
         self,
         text: str,
-        conditioning_speech: Float[Tensor, "n spec_d l"],
+        conditioning_speech: str,
         samples_to_generate: int = 1,
     ) -> Tensor:
         input_ids: List[int] = self.tokenizer.encode(text)
         text_inputs: Int[Tensor, "1 l"] = torch.tensor(input_ids).unsqueeze(0)
-        conditioning_latent = self.conditioning_encoder(speech=conditioning_speech)
+
+        torch.manual_seed(1)
+        
+        conditioning_latent = self.conditioning_encoder.get_conditioning(speech_wav=conditioning_speech)
+        #print("auto cond latent:", conditioning_latent)
+        #print("diffusion cond latent:", torch.load("cond_latent.pth"))
 
         print(f"Generating {samples_to_generate} samples...")
         samples = []
         for i in range(samples_to_generate):
-            print(f"Generating sample nr {i} of {samples_to_generate}")
+            print(f"Generating sample nr {i+1} of {samples_to_generate}")
             sample_speech_tokens: Int[Tensor, "1 length_speech_output"] = self.autoregressive.generate_speech_tokens(
                 text_inputs, speech_conditioning_latent=conditioning_latent  # , max_length=80  # for testing
             )
+            #sample_speech_tokens = torch.load("codes.pth")
             samples.append(sample_speech_tokens)
 
         print("Ranking samples...")
@@ -55,7 +62,7 @@ class Inference:
         # print(best_result)
 
         print("Generating latent...")
-        _loss_text, _loss_speech, _speech_logits, latent = self.autoregressive(
+        latent = self.autoregressive(
             text_inputs=text_inputs,
             speech_inputs=best_result,
             speech_conditioning_latent=conditioning_latent,
@@ -72,36 +79,39 @@ class Inference:
             else:
                 c_token_cnt = 0
 
+        print("latent:", latent)
         print("Generating mel spectrogram...")
         # diffusion
         output_len = (
             latent.shape[1] * 4 * 24_000 // 22_050
         )  # This diffusion model converts from 22kHz spectrogram codes to a 24kHz spectrogram signal.
-        diffusion_temperature = 1  # TODO: don't hardcode
+        diffusion_temperature = 1.0  # TODO: don't hardcode
         noise = torch.randn(latent.shape[0], 100, output_len) * diffusion_temperature
+        #print("orig noise:", noise)
         timestep_independent = self.diffuser.independent_timestep(latent, output_len)
+        #print("indep t:", timestep_independent)
         mel = self.diffuser.sample(noise=noise, embeddings=timestep_independent)
 
         MEL_MAX = 2.3143386840820312
         MEL_MIN = -11.512925148010254
         mel = ((mel + 1) / 2) * (MEL_MAX - MEL_MIN) + MEL_MIN  # denormalize
         mel = mel[:, :, :output_len]
-
+        #print("mel:", mel)
         print("Generating waveform...")
         wav = self.vocoder.inference(mel)
+        #print("wav:",wav)
         return wav
 
 
 if __name__ == "__main__":
     import torchaudio
 
-    torch.manual_seed(1)
     torch.set_default_device("cuda")  # type: ignore[no-untyped-call]
     inference = Inference()
     wav_tensor = inference(
-        "TorToiSe is a text-to-speech program that is capable of synthesizing speech "
+        "Tortoise is a text-to-speech program that is capable of synthesizing speech "
         "in multiple voices with realistic prosody and intonation.",
-        conditioning_speech=torch.randn(1, 80, 3272),
+        conditioning_speech="emma.wav",
     ).cpu()
     print(wav_tensor.shape)
     print("Saving output to test.wav...")
